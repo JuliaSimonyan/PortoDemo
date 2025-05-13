e46d65d99b4d8c62907e5807655e5ce48ad72adc
Inesa Mkrtchyan <inesamkrtchyan9999@gmail.com>
2025-05-14 00:47:46 +0400

    a

---
+++ Porto/Controllers/StaticChatBotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Porto.Controllers
+{
+    public class StaticChatBotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
